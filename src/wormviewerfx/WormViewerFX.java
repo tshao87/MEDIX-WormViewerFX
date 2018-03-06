bdbb90421445d5a9c265621ddffb22551be0c2a7
MSHAO1 <MSHAO1@CDM-CD720RES03.dpu.depaul.edu>
2018-03-06 11:38:48 -0600

    FX version static data panel

--- src/wormviewerfx/WormViewerFX.java
+++ src/wormviewerfx/WormViewerFX.java
@@ -17,10 +17,11 @@ public class WormViewerFX extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("WormViewFXMain.fxml"));
         
         Scene scene = new Scene(root);
         
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/wormviewerfx/WormViewerFXMainController.java
@@ -0,0 +1,25 @@
+
+package wormviewerfx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author MSHAO1
+ */
+
+
+public class WormViewerFXMainController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
